b950e713cb919c76dc2e34cf02525b9866a54f86
pintor.dev <pintor.dev@gmail.com>
2024-05-02 22:37:33 +0900

    level #3 42898 refactor

--- src/main/java/level3/Solution_12907.java
+++ src/main/java/level3/Solution_12907.java
@@ -5,6 +5,7 @@ class Solution_12907 {
     public static void main(String[] args) {
 
         Solution_12907 s = new Solution_12907();
+
         int n = 5;
         int[] money = {1, 2, 5};
         int result = 4;
--- src/main/java/level3/Solution_42898.java
+++ src/main/java/level3/Solution_42898.java
@@ -15,19 +15,19 @@ class Solution_42898 {
 
     public int solution(int m, int n, int[][] puddles) {
 
-        int[][] map = new int[m][n];
-        for (int[] puddle : puddles) map[puddle[0] - 1][puddle[1] - 1] = -1;
+        int[][] map = new int[m + 1][n + 1];
+        for (int[] puddle : puddles) map[puddle[0]][puddle[1]] = -1;
 
-        map[0][0] = 1;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        map[1][1] = 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
                 if (map[i][j] == -1) continue;
-                if (i > 0 && map[i - 1][j] != -1) map[i][j] += map[i - 1][j];
-                if (j > 0 && map[i][j - 1] != -1) map[i][j] += map[i][j - 1];
+                map[i][j] += map[i - 1][j] != -1 ? map[i - 1][j] : 0;
+                map[i][j] += map[i][j - 1] != -1 ? map[i][j - 1] : 0;
                 map[i][j] %= 1000000007;
             }
         }
 
-        return map[m - 1][n - 1];
+        return map[m][n];
     }
 }
