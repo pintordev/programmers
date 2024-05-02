2e67a281fd8bc5bf0f3b2d67facf7a980270b999
pintor.dev <pintor.dev@gmail.com>
2024-05-02 22:32:50 +0900

    level #3 12907

---
+++ src/main/java/level3/Solution_12907.java
@@ -0,0 +1,27 @@
+package level3;
+
+class Solution_12907 {
+
+    public static void main(String[] args) {
+
+        Solution_12907 s = new Solution_12907();
+        int n = 5;
+        int[] money = {1, 2, 5};
+        int result = 4;
+        System.out.println(s.solution(n, money) == result);
+    }
+
+    public int solution(int n, int[] money) {
+
+        int[] memo = new int[n + 1];
+        memo[0] = 1;
+
+        int len = money.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = money[i]; j <= n; j++) {
+                memo[j] += memo[j - money[i]];
+            }
+        }
+        return memo[n];
+    }
+}
