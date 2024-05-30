e4021f82d9c862ef4de8722c6474291cc5257ab1
pintor.dev <pintor.dev@gmail.com>
2024-05-30 17:55:33 +0900

    level #3 1832 refactor

--- src/main/java/level3/solution_1832/Solution_1832.java
+++ src/main/java/level3/solution_1832/Solution_1832.java
@@ -19,20 +19,12 @@ class Solution_1832 {
 
     int MOD = 20170805;
     public int solution(int m, int n, int[][] cityMap) {
-
-        int[][] expendedCityMap = new int[m + 1][n + 1];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                expendedCityMap[i + 1][j + 1] = cityMap[i][j];
-            }
-        }
-
         int[][][] memo = new int[2][m + 1][n + 1];
         memo[0][1][1] = 1;
         memo[1][1][1] = 1;
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                int f = expendedCityMap[i][j];
+                int f = cityMap[i - 1][j - 1];
                 if (f == 2) {
                     memo[0][i][j] = memo[0][i - 1][j];
                     memo[1][i][j] = memo[1][i][j - 1];
