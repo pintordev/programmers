491f7cfa0c9e93263a82223a1f0365102e91631e
pintor.dev <pintor.dev@gmail.com>
2024-07-19 16:55:39 +0900

    level #4 60060 sol2

--- src/main/java/level4/solution_60060/Solution_60060.java
+++ src/main/java/level4/solution_60060/Solution_60060.java
@@ -1,8 +1,6 @@
 package level4.solution_60060;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Solution_60060 {
 
@@ -14,9 +12,11 @@ class Solution_60060 {
         String[] queries = {"fro??", "????o", "fr???", "fro???", "pro?"};
         int[] result = {3, 2, 4, 1, 0};
 
-        for (int i = 0, t = result.length; i < t; i++) {
-            System.out.println(Arrays.equals(s.solution(words, queries), result));
-        }
+        System.out.println("===== solution 1 =====");
+        System.out.println(Arrays.equals(s.solution(words, queries), result));
+
+        System.out.println("===== solution 2 =====");
+        System.out.println(Arrays.equals(s.solution2(words, queries), result));
     }
 
     public int[] solution(String[] words, String[] queries) {
@@ -34,6 +34,50 @@ class Solution_60060 {
             else result[i] = back.search(reverse(queries[i]));
         }
         return result;
+    }
+
+    public int[] solution2(String[] words, String[] queries) {
+        Map<Integer, List<String>> front = new HashMap<>();
+        Map<Integer, List<String>> back = new HashMap<>();
+        for (String word : words) {
+            int wLen = word.length();
+            front.computeIfAbsent(wLen, k -> new ArrayList<>()).add(word);
+            back.computeIfAbsent(wLen, k -> new ArrayList<>()).add(reverse(word));
+        }
+
+        for(int key : front.keySet()) {
+            front.get(key).sort(String::compareTo);
+            back.get(key).sort(String::compareTo);
+        }
+
+        int qLen = queries.length;
+        int[] result = new int[qLen];
+        for (int i = 0; i < qLen; i++) {
+            if (queries[i].charAt(0) != '?') result[i] = search(front, queries[i]);
+            else result[i] = search(back, reverse(queries[i]));
+        }
+        return result;
+    }
+
+    public int search(Map<Integer, List<String>> list, String query) {
+        int len = query.length();
+        if (!list.containsKey(len)) return 0;
+
+        String ls = query.replace('?', 'a');
+        String us = query.replace('?', 'z');
+
+        return binarySearch(list.get(len), us) - binarySearch(list.get(len), ls);
+    }
+
+    public int binarySearch(List<String> words, String str) {
+        int low = -1;
+        int high = words.size();
+        while (low + 1 < high) {
+            int mid = (low + high) >> 1;
+            if (words.get(mid).compareTo(str) <= 0) low = mid;
+            else high = mid;
+        }
+        return high;
     }
 
     public String reverse(String str) {
